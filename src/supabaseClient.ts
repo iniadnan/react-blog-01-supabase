89710b904bcbb1ad03bad840eeefae8d7ad1d420
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-09-23 15:20:25 +0700

    move key and url to env

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // WILL BE USE .ENV LATER!
-const supabaseUrl: string = "https://vfkbqzbgkvktbyiudlay.supabase.co"
-const supabaseKey: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZma2JxemJna3ZrdGJ5aXVkbGF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI2Njg1NTYsImV4cCI6MjAwODI0NDU1Nn0.vn8gN0sc8HAVTAnuOUwsATCtPj68mMUiQQ_EMtJfdBk"
+const supabaseUrl: string = import.meta.env.VITE_SUPBASE_URL
+const supabaseKey: string = import.meta.env.VITE_SUPBASE_KEY
 
 const SUPABASE = createClient(supabaseUrl, supabaseKey)
 
